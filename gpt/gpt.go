07ec625cbcb03a183f916fac73adc84017cb036c
yanghaolin <yanghaolin@xiaomi.com>
2023-03-16 12:46:12 +0800

    [mod] extend http timeout

--- gpt/gpt.go
+++ gpt/gpt.go
@@ -73,7 +73,7 @@ func Completions(msg string) (string, error) {
 	apiKey := config.LoadConfig().ApiKey
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+apiKey)
-	client := &http.Client{Timeout: 30 * time.Second}
+	client := &http.Client{Timeout: 60 * time.Second}
 	response, err := client.Do(req)
 	if err != nil {
 		return "", err
